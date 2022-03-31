3d538562dc40ce2058d3b8d8734e62511ff4fb00
oseven <90019912+oseven07@users.noreply.github.com>
2022-03-31 19:57:42 -0400

    solved state

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,56 @@
+/* eslint-disable indent */
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [str_hol, set_str_hol] = useState<string>("Christmas");
+
+    const holiday = [
+        "New Years",
+        "Presidents Day",
+        "Independence Day",
+        "Thanks Giving",
+        "Christmas"
+    ];
+    const emoji = ["🎊", "🥳", "👤", "🐔", "🎅🏼"];
+
+    function next_alphabet(): void {
+        switch (str_hol.at(0)) {
+            case "C":
+                set_str_hol("Independence Day");
+                break;
+
+            case "I":
+                set_str_hol("New Years");
+                break;
+
+            case "N":
+                set_str_hol("Presidents Day");
+                break;
+
+            case "P":
+                set_str_hol("Thanks Giving");
+                break;
+
+            case "T":
+                set_str_hol("Christmas");
+                break;
+        }
+    }
+
+    function next_year(): void {
+        set_str_hol(
+            holiday[
+                (holiday.findIndex((str: string) => str_hol == str) + 1) % 5
+            ]
+        );
+    }
+
+    return (
+        <div>
+            Cycle Holiday<Button onClick={next_alphabet}>Next Alphabet</Button>
+            <Button onClick={next_year}>Next Year</Button>
+            Holiday: {emoji[holiday.findIndex((str: string) => str_hol == str)]}
+        </div>
+    );
 }
