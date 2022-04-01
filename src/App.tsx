60940ef5d536498c4ae6ea6ecc0ed79133eeae5e
oseven <90019912+oseven07@users.noreply.github.com>
2022-03-31 20:11:34 -0400

    state changes added

--- src/App.tsx
+++ src/App.tsx
@@ -61,21 +61,23 @@ function App(): JSX.Element {
                     </Row>
                 </Container>
             </header>
-<<<<<<< HEAD
-=======
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
->>>>>>> solved-state
+            <div className="App">
+                <header className="App-header">
+                    UD CISC275 with React Hooks and TypeScript
+                </header>
+                <hr></hr>
+                <Counter></Counter>
+                <hr />
+                <RevealAnswer></RevealAnswer>
+                <hr />
+                <StartAttempt></StartAttempt>
+                <hr />
+                <TwoDice></TwoDice>
+                <hr />
+                <ChangeType></ChangeType>
+                <hr />
+                <CycleHoliday></CycleHoliday>
+            </div>
         </div>
     );
 }
