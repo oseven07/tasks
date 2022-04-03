efeaa19c2bba0c746067700be863918648f42c27
oseven <90019912+oseven07@users.noreply.github.com>
2022-04-03 19:29:37 -0400

    main

--- src/App.tsx
+++ src/App.tsx
@@ -6,78 +6,55 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-
-import { Button } from "react-bootstrap";
-import { Container } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
+import { DoubleHalf } from "./bad-components/DoubleHalf";
+import { ColoredBox } from "./bad-components/ColoredBox";
+import { ShoveBox } from "./bad-components/ShoveBox";
+import { ChooseTeam } from "./bad-components/ChooseTeam";
+import { CheckAnswer } from "./form-components/CheckAnswer";
+import { GiveAttempts } from "./form-components/GiveAttempts";
+import { EditMode } from "./form-components/EditMode";
+import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>L&apos;Evan Brady</h1>
                 UD CISC275 with React Hooks and TypeScript
-                <p>Hello World</p>
-                <p>
-                    Edit <code>src/App.tsx</code> and save. This page will
-                    automatically reload.
-                </p>
-                <ol>
-                    <li>uno</li>
-                    <li>dos</li>
-                    <li>tres</li>
-                </ol>
-                <img
-                    src="https://i5.walmartimages.com/asr/57e81448-5c10-455f-a733-6c031e020890.78e0baa09a9768621b35908f4cd420ec.jpeg"
-                    alt="A desert painting."
-                />
-                <Button
-                    className="btn btn-primary"
-                    onClick={() => console.log("Hello World!")}
-                >
-                    Log Hello World
-                </Button>
-                <Container>
-                    <Row>
-                        <Col>
-                            <div
-                                style={{
-                                    width: "200px",
-                                    height: "100px",
-                                    backgroundColor: "red"
-                                }}
-                            ></div>
-                        </Col>
-                        <Col>
-                            <div
-                                style={{
-                                    width: "400px",
-                                    height: "100px",
-                                    backgroundColor: "red"
-                                }}
-                            ></div>
-                        </Col>
-                    </Row>
-                </Container>
             </header>
-            <div className="App">
-                <header className="App-header">
-                    UD CISC275 with React Hooks and TypeScript
-                </header>
-                <hr></hr>
-                <Counter></Counter>
-                <hr />
-                <RevealAnswer></RevealAnswer>
-                <hr />
-                <StartAttempt></StartAttempt>
-                <hr />
-                <TwoDice></TwoDice>
-                <hr />
-                <ChangeType></ChangeType>
-                <hr />
-                <CycleHoliday></CycleHoliday>
-            </div>
+            <hr></hr>
+            <CheckAnswer expectedAnswer="42"></CheckAnswer>
+            <hr></hr>
+            <GiveAttempts></GiveAttempts>
+            <hr></hr>
+            <EditMode></EditMode>
+            <hr></hr>
+            <ChangeColor></ChangeColor>
+            <hr></hr>
+            <MultipleChoiceQuestion
+                options={["a", "b", "c"]}
+                expectedAnswer="b"
+            ></MultipleChoiceQuestion>
+            <hr></hr>
+            <DoubleHalf></DoubleHalf>
+            <hr></hr>
+            <ChooseTeam></ChooseTeam>
+            <hr></hr>
+            <ColoredBox></ColoredBox>
+            <hr></hr>
+            <ShoveBox></ShoveBox>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
