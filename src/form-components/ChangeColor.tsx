0388b75870773b328d63c6e0a7de9f2d019e2c35
oseven <90019912+oseven07@users.noreply.github.com>
2022-04-03 19:20:19 -0400

    solved forms

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>("red");
+
+    const COLORS = [
+        "red",
+        "orange",
+        "brown",
+        "yellow",
+        "green",
+        "blue",
+        "purple",
+        "black"
+    ];
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((arr_ele: string) => (
+                <li key={arr_ele}>
+                    <Form.Check
+                        inline
+                        type="radio"
+                        checked={arr_ele == color}
+                        label={arr_ele}
+                        color={arr_ele}
+                        value={arr_ele}
+                        onChange={updateColor}
+                    ></Form.Check>
+                </li>
+            ))}
+            <span data-testid="colored-box" style={{ backgroundColor: color }}>
+                {color}
+            </span>{" "}
+            selected
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,37 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemps, setAttemps] = useState<number>(3);
+    const [text, setText] = useState<string>("");
+    function useButton() {
+        setAttemps(attemps - 1);
+    }
+
+    function updateAttemps(event: React.ChangeEvent<HTMLInputElement>) {
+        setText(event.target.value);
+    }
+
+    function gainButton() {
+        const textInput = parseInt(text);
+        if (!isNaN(textInput)) {
+            setAttemps(attemps + textInput);
+        }
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Form.Control
+                type="number"
+                onChange={updateAttemps}
+                placeholder="How many more attempts do you want"
+            ></Form.Control>
+            <p>{attemps} left.</p>
+            <button onClick={useButton} disabled={attemps < 1}>
+                use
+            </button>
+            <button onClick={gainButton}>gain</button>
         </div>
     );
 }
